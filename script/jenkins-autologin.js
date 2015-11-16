4e38836136bfc00fc0668be238af924c66ad692c
Petr Chromec <petr.chromec@lmc.eu>
2015-11-16 14:38:21 +0100

    jenkin autoloading update

--- script/jenkins-autologin.js
+++ script/jenkins-autologin.js
@@ -4,11 +4,13 @@ console.log('jenkins-autologin');
         
         var location = window.location.href;
         
-        if (location.indexOf('/login')) {
-            // login
-            jQuery('form[name="login"]').submit();
+        if (location.indexOf('/login') > 0) {
+            // login - nejde protoze form nema bez kliku uzivatele udaje ulozene v browseru
+                //jQuery('form[name="login"] input:first').click().focus().click();
+                //jQuery(jQuery('form[name="login"] input')[1]).focus().click().focus().val();
         } else if (jQuery('.login').length) {
             // to login page
+            // todo - osetrit odhlaseni (ma stejny selector...)
             jQuery('.login a b').click();
         }
     });
