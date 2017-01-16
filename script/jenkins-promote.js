04060a03bcefeb6cd66d5dfe6162e00c3132734a
Petr Chromec <mortal_flesh@seznam.cz>
2017-01-16 15:33:00 +0100

    Jenkins promote script updated to be more flexibile with new jenkins

--- script/jenkins-promote.js
+++ script/jenkins-promote.js
@@ -1,70 +1,100 @@
 console.log('jenkins-promote');
-(function ($) {
-    $(document).ready(function () {
 
-        if ($('#buildHistory').length > 0) {
-            console.log('jenkins-promote-ready');
+(function (w) {
+    function initJenkinsPromote($) {
+        $(document).ready(function () {
 
-            window.custom_jqUiLoaded = false;
+            if ($('#buildHistory').length > 0) {
+                console.log('jenkins-promote-ready');
 
-            function promotionDialog(baseUrl, response) {
-                var $promotion = $('<iframe id="custom-promotion-id" height="400" width="280"></iframe>')
-                    .attr('src', baseUrl + 'promotion/');
+                window.custom_jqUiLoaded = false;
 
-                var $promotionContainer = $('#custom-promotion-container');
-                $promotionContainer
-                    .empty()
-                    .append($promotion)
-                    .dialog();
+                function promotionDialog(baseUrl, response) {
+                    var $promotion = $('<iframe id="custom-promotion-id" height="400" width="280"></iframe>')
+                        .attr('src', baseUrl + 'promotion/');
 
-                setTimeout(function () {
-                    scrollToEndOfIframe('#custom-promotion-id');
-                }, 1000);
-            }
+                    var $promotionContainer = $('#custom-promotion-container');
+                    $promotionContainer
+                        .empty()
+                        .append($promotion)
+                        .dialog();
 
-            function scrollToEndOfIframe(iframeSelector) {
-                var $contents = $(iframeSelector).contents();
-
-                $contents.scrollTop($contents.height());
-            }
-
-            function showPromotion() {
-                if (window.custom_jqUiLoaded) {
-                    promotionDialog(baseUrl);
-                } else {
-                    $.getScript('//code.jquery.com/ui/1.11.4/jquery-ui.js', function () {
-                        console.log('jQuery-UI loaded');
-                        window.custom_jqUiLoaded = true;
-
-                        promotionDialog(baseUrl);
-                    });
+                    setTimeout(function () {
+                        scrollToEndOfIframe('#custom-promotion-id');
+                    }, 1000);
                 }
+
+                function scrollToEndOfIframe(iframeSelector) {
+                    var $contents = $(iframeSelector).contents();
+
+                    $contents.scrollTop($contents.height());
+                }
+
+                function showPromotion() {
+                    if (window.custom_jqUiLoaded) {
+                        promotionDialog(baseUrl);
+                    } else {
+                        $.getScript('//code.jquery.com/ui/1.11.4/jquery-ui.js', function () {
+                            console.log('jQuery-UI loaded');
+                            window.custom_jqUiLoaded = true;
+
+                            promotionDialog(baseUrl);
+                        });
+                    }
+                }
+
+                window.showPromotion = showPromotion;
+
+                var $build = $('.pane.build-name:first', '#buildHistory');
+                var $buildLinks = $build.find('a');
+                var $buildConsoleLink = $buildLinks.length > 1 ? $buildLinks.first() : $buildLinks;
+
+                var consoleUrl = $buildConsoleLink.attr('href');
+                var baseUrl = consoleUrl.substr(0, consoleUrl.length - 'console'.length);   // fe: /view/EDU/job/vysokeskoly.cz/36/
+                var buildNumber = $build.text().replace(/[^\d]*/g, '');
+
+                var $button = $('<button type="button" onclick="showPromotion()"></button>')
+                    .text('#' + buildNumber)
+                    .attr('title', 'Promotion')
+                    .css({cursor: 'pointer'});
+                var $spanWithButton = $('<span></span>').append($button);
+
+                var $container = $('<div id="custom-promotion-container"></div>')
+                    .hide()
+                    .css({border: '3px solid red', borderRadius: '10px', padding: '10px', background: '#fff'});
+
+                $build
+                    .empty()
+                    .append($buildConsoleLink)
+                    .append($spanWithButton)
+                    .append($container);
             }
+        });
+    }
 
-            window.showPromotion = showPromotion;
+    function loadScript(url, callback) {
+        // Adding the script tag to the head as suggested before
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
 
-            var $build = $('.pane.build-name:first', '#buildHistory');
-            var $buildConsole = $build.find('a');
+        // Then bind the event to the callback function.
+        // There are several events for cross browser compatibility.
+        script.onreadystatechange = callback;
+        script.onload = callback;
 
-            var consoleUrl = $buildConsole.attr('href');
-            var baseUrl = consoleUrl.substr(0, consoleUrl.length - 'console'.length);   // fe: /view/EDU/job/vysokeskoly.cz/36/
-            var buildNumber = $build.text().replace(/[^\d]*/g, '');
+        // Fire the loading
+        head.appendChild(script);
+    }
 
-            var $button = $('<button type="button" onclick="showPromotion()"></button>')
-                .text('#' + buildNumber)
-                .attr('title', 'Promotion')
-                .css({cursor: 'pointer'});
-            var $span = $('<span></span>').append($button);
-
-            var $container = $('<div id="custom-promotion-container"></div>')
-                .hide()
-                .css({border: '3px solid red', borderRadius: '10px', padding: '10px', background: '#fff'});
-
-            $build
-                .empty()
-                .append($buildConsole)
-                .append($span)
-                .append($container);
-        }
-    });
-})(jQuery);
+    if (w.jQuery === undefined) {
+        loadScript('//code.jquery.com/jquery-1.12.4.min.js', function () {
+            w.jQuery.noConflict();
+            console.log('jenkins-promote: jQuery loaded');
+            initJenkinsPromote(w.jQuery);
+        });
+    } else {
+        initJenkinsPromote(w.jQuery);
+    }
+})(window);
