64bc3c49fd735e558781c43bd0ba3af7fa48d6de
Petr Chromec <mortal_flesh@seznam.cz>
2017-11-14 14:53:05 +0100

    update storm shell

---
+++ phpstorm/updateStorm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p templates
+cp ./templates/* ~/Library/Preferences/PhpStorm2017.2/templates
+
+mkdir -p fileTemplates/code
+cp ./fileTemplates/code/* ~/Library/Preferences/PhpStorm2017.2/fileTemplates/code
+
+mkdir -p fileTemplates/includes
+cp ./fileTemplates/includes/* ~/Library/Preferences/PhpStorm2017.2/fileTemplates/includes
