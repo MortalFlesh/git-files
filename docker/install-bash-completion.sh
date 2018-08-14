711ef5aee817ac0126e992827ba524784bbd4c1f
Petr Chromec <mortal_flesh@seznam.cz>
2018-08-14 17:42:30 +0200

    Add script for install docker completion

---
+++ docker/install-bash-completion.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# https://docs.docker.com/docker-for-mac/#install-shell-completion
+etc=/Applications/Docker.app/Contents/Resources/etc
+ln -s $etc/docker.bash-completion $(brew --prefix)/etc/bash_completion.d/docker
+ln -s $etc/docker-machine.bash-completion $(brew --prefix)/etc/bash_completion.d/docker-machine
+ln -s $etc/docker-compose.bash-completion $(brew --prefix)/etc/bash_completion.d/docker-compose
+
+echo "linked..."
