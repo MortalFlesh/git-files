7a0c6fe524f6635eeba258b6d9c65ac0ca6d795f
Petr Chromec <mortal_flesh@seznam.cz>
2019-02-28 15:37:28 +0100

    Make links in template clickable

--- script/narwhal.js
+++ script/narwhal.js
@@ -9,14 +9,15 @@ console.log('Narwhal scripts');
         // Configurations, selectors, ...
         //
 
-        const id = {
+        const selector = {
             editButton: { id: 'n-edit-button', selector: '#n-edit-button' },
-            description: { id: 'n-description-row', selector: '#n-description-row' },
+            clickable: { class: 'n-clickable', selector: '.n-clickable' },
         }
 
         const selectors = [
-            { title: 'edit button', selector: 'a[href^="/edit"]:first', attr: [ 'id', id.editButton.id ]},
-            { title: 'description row', selector: 'tr:contains(description):first > td:first', attr: [ 'id', id.description.id ]},
+            { title: 'edit button', selector: 'a[href^="/edit"]:first', attr: ['id', selector.editButton.id] },
+            { title: 'description row', selector: 'tr:contains("description"):first > td:first', attr: ['class', selector.clickable.class] },
+            { title: 'template row', selector: 'tr:contains("url template"):first > td:first', attr: ['class', selector.clickable.class] },
         ]
 
         // distribute attributes to elements by selectors above
@@ -40,21 +41,24 @@ console.log('Narwhal scripts');
         // edit on `E` keyup
         $('body').on('keyup', ({ key }) => {
             if (key === 'e') {
-                ifExistsDo($(id.editButton.selector), ($edit) => {
+                ifExistsDo($(selector.editButton.selector), ($edit) => {
                     window.location.href = $edit.first().attr('href')
                 })
             }
         })
 
         // make links in description clickable
-        ifExistsDo($(id.description.selector), ($description) => {
-            const content =
-                $description
-                    .html()
-                    .split(' ')
-                    .map((word) => word.startsWith('http') ? `<a href="${word}" target="_blank">${word}</a>` : word)
-                    .join(' ')
-            $description.html(content)
+        ifExistsDo($(selector.clickable.selector), ($clickable) => {
+            $clickable.each((_, item) => {
+                const $item = $(item)
+                const content =
+                    $item
+                        .html()
+                        .split(' ')
+                        .map((word) => word.startsWith('http') ? `<a href="${word}" target="_blank">${word}</a>` : word)
+                        .join(' ')
+                $item.html(content)
+            })
         })
 
     });
