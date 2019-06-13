771bc6f0b975eba2c526b10620a93f7355931d30
Petr Chromec <mortal_flesh@seznam.cz>
2019-06-13 09:46:47 +0200

    Add artifact dashboard overview for narwhal

--- script/narwhal.js
+++ script/narwhal.js
@@ -41,10 +41,14 @@ console.log('Narwhal scripts');
             jinja2UrlTemplate: { id: 'n-jinja2-urlTemplate', selector: '#n-jinja2-urlTemplate' },
             metaData: { id: 'n-meta', selector: '#n-meta' },
             metaDataType: { id: 'n-meta-type', selector: '#n-meta-type' },
+            artifactItem: { class: 'n-artifact-item', selector: '.n-artifact-item' },
+            artifactItemId: { id: 'n-artifact-id', selector: '#n-artifact-id-' },
         }
 
         const selectors = [
             { title: 'edit button', selector: 'a[href^="/edit"]:first', attr: { id: selector.editButton.id } },
+            { title: 'artifact item', selector: '.tbl-artifact tr', attr: { class: selector.artifactItem.class } },
+            { title: 'artifact item id', selector: '.tbl-artifact tr', attr: { id: selector.artifactItemId.id, data: 'id' } },
 
             // clickable content
             { title: 'description row', selector: 'tr:contains("description"):first > td:first', attr: { class: selector.clickable.class } },
@@ -63,6 +67,12 @@ console.log('Narwhal scripts');
             ifExistsDo($(selector), ($item) => {
                 if (attr.hasOwnProperty('class')) {
                     $item.addClass(attr.class)
+                } else if (attr.hasOwnProperty('id') && attr.hasOwnProperty('data')) {
+                    $item.each((_k, singleItem) => {
+                        const $singleItem = $(singleItem)
+                        const id = `${attr.id}-${$singleItem.data(attr.data)}`
+                        $singleItem.attr('id', id)
+                    })
                 } else if (attr.hasOwnProperty('id')) {
                     $item.attr('id', attr.id)
                 }
@@ -161,5 +171,73 @@ console.log('Narwhal scripts');
                 .catch((e) => console.error(e))
         }
 
+
+        // add dashboard overview
+        if (window.location.href.indexOf('/artifacts') > 0 && $(selector.artifactItem).length > 0) {
+            $('.n-overview').remove()
+
+            const example = [
+                { id: 923, configurationId: 953, alias: 'thor (router)' },
+                { id: 962, configurationId: 963, alias: 'rogue (person-filter)' },
+                { id: 924, configurationId: 952, alias: 'ant-man (aggregator)' },
+                { id: 965, configurationId: 966, alias: 'quicksilver (deriver)' },
+                { id: 958, configurationId: 959, alias: 'hulk (data_processing-filter)' },
+                { id: 960, configurationId: 961, alias: 'she-hulk (all-intent-filter)' },
+                { id: 904, configurationId: 968, alias: 'loki (stator)' },
+            ]
+
+            const dashboardArtifactsOverview = localStorage.hasOwnProperty('dashboardArtifactsOverview')
+                ? JSON.parse(localStorage.getItem('dashboardArtifactsOverview'))
+                : []
+
+            const $getArtifact = (id) => {
+                const $artifact = $(`${selector.artifactItemId.selector}${id}`)
+
+                return $artifact.length
+                    ? $artifact
+                    : $('<tr><td>id</td></tr>') // not all artifacts are shown on the page, but we can create a link
+                        .append($(`<td><a href="/artifacts/${id}">artefact id: ${id}</a></td>`))
+            }
+
+            const $createAlias = (alias) => alias.length > 0
+                ? $('<h3></h3>')
+                    .text(alias)
+                    .css({ fontSize: 12, margin: 0, textAlign: 'right' })
+                : ''
+
+            const $createArtifact = ($artifact) =>
+                $('<h4></h4>')
+                    .append($artifact.clone().find('td:nth-child(2)'))
+                    .css({ fontSize: 12 })
+
+            const $createConfiguration = ($configuration) => $configuration
+                ? $('<h5></h5>')
+                    .append($configuration.clone().find('td:nth-child(2)'))
+                    .css({ fontSize: 11 })
+                : ''
+
+            const $createOverviewItem = ($artifact, $configuration, alias) =>
+                $('<div class="n-overview-item"></div>')
+                    .append($createAlias(alias))
+                    .append($createArtifact($artifact))
+                    .append($createConfiguration($configuration))
+                    .css({ margin: '0 10px 10px 0', padding: 5, border: '1px solid black', borderRadius: 5, height: 80, float: 'left' })
+
+            const $overview = $('<div class="n-overview"></div>').css({ paddingTop: 10, paddingBottom: 10 })
+
+            dashboardArtifactsOverview.forEach(({ id, configurationId = 0, alias = '' }) => {
+                $overview.append(
+                    $createOverviewItem(
+                        $getArtifact(id),
+                        $getArtifact(configurationId),
+                        alias
+                    )
+                )
+            })
+
+            $('h3:first').append(
+                $overview.append($('<br style="clear: both">'))
+            )
+        }
     });
 })(window.jQuery);
