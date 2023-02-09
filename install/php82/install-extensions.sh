e513f9a5ebc7b772590c5ab95830ea9d31a9260b
Petr Chromec <mortal_flesh@seznam.cz>
2023-02-09 09:52:18 +0100

    Add php82 install scripts

---
+++ install/php82/install-extensions.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# include functions
+. ~/www/git-files/functions.sh
+
+set -e  # fail on error
+
+title "[PHP@8.2] - install extensions"
+echo ""
+echo "----"
+echo "NOTE: keep in mind that php extensions need other libraries, which may be installed by 'brew install ...'"
+echo "----"
+echo ""
+
+title "Prepare pear tmp dir"
+mkdir -p /private/tmp/pear/temp
+sudo chown -R "$(whoami):wheel" /private/tmp/pear
+sudo chmod -R 0777 /private/tmp/pear
+
+title "Update pecl"
+pecl channel-update pecl.php.net
+
+title "Install extensions"
+#pecl install rdkafka
+#pecl install igbinary
+
+#pip3 install fastlz
+#pecl install memcached
+
+pecl install xdebug
+
+title "[PHP@8.2] - installed extensions"
+pecl list
+
+title "[PHP@8.2] - Done!"
+php --version
---
+++ install/php82/install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# include functions
+. ~/www/git-files/functions.sh
+
+title "[PHP@8.2] - installs"
+brew install php@8.2 # or just php
+
+title "[PHP@8.2] - Backup originals"
+mkdir -p ~/www/git-files/install/php82/backup
+cp /usr/local/etc/php/8.2/php.ini ~/www/git-files/install/php82/backup/
+cp /usr/local/etc/php/8.2/php-fpm.d/www.conf ~/www/git-files/install/php82/backup/
+
+title "[PHP@8.2] - Symlink ini"
+# - use ln -s -f ... will override files in your home
+ls -FGlh /usr/local/etc/php/8.2/php.ini
+ln -s -f ~/www/git-files/install/php82/php.ini /usr/local/etc/php/8.2/php.ini
+ls -FGlh /usr/local/etc/php/8.2/php.ini
+
+title "[PHP@8.2] - Symlink FPM config"
+echo "-> skipped"
+# - use ln -s -f ... will override files in your home
+# ls -FGlh /usr/local/etc/php/8.2/php-fpm.d/www.conf
+# ln -s -f ~/www/git-files/install/php82/www.conf /usr/local/etc/php/8.2/php-fpm.d/www.conf
+# ls -FGlh /usr/local/etc/php/8.2/php-fpm.d/www.conf
+
+title "[PHP@8.2] - Run FPM"
+echo "-> skipped"
+#sudo brew services start php
+
+title "[PHP@8.2] - Done!"
+php --version
