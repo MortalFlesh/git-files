e2312ab04af493b4d650758d16b7ccde523e8375
Petr Chromec <mortal_flesh@seznam.cz>
2023-05-10 16:19:42 +0200

    Update install scripts

--- install/install.sh
+++ install/install.sh
@@ -6,9 +6,9 @@ set -e
 . ~/www/git-files/functions.sh
 
 # install brew
-. ~/www.git-files/install/install-brew.sh
-. ~/www.git-files/install/install-tools.sh
-. ~/www.git-files/install/install-profiles.sh
+. ~/www/git-files/install/install-brew.sh
+. ~/www/git-files/install/install-tools.sh
+. ~/www/git-files/install/install-profiles.sh
 
 title "Done!"
 echo "Now restart terminal and run other installations."
