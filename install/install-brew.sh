70882521cd73c4f105b48472fb0c4262b60f1541
Petr Chromec <mortal_flesh@seznam.cz>
2024-02-05 13:29:51 +0100

    Install orbstack from pkg/dmg

--- install/install-brew.sh
+++ install/install-brew.sh
@@ -20,7 +20,9 @@ brew install wget bash-completion git gnupg gnutls gpgme hadolint htop libmemcac
     libusb libxml2 libyaml libzip markdown memcached midnight-commander mysql nuget openldap openssl p7zip \
     postgresql@14 pure-ftpd python ruby shellcheck sqlite tree zlib curl pkg-config \
     certbot consul-template helm jq pinentry-mac imagemagick kubectx kubernetes-cli kcat fzf thefuck \
-    1password-cli orbstack ykman
+    1password-cli ykman
+
+# orbstack - is installed from pkg/dmg
 
 mkdir -p ~/bin
 
